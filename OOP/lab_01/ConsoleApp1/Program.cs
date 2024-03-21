1653d8d040c7cb2b046d89f2794ae3d63807658d
systnager <systnager@gmail.com>
2024-03-21 04:36:44 +0200

    complete console task 1

--- OOP/lab_01/ConsoleApp1/Program.cs
+++ OOP/lab_01/ConsoleApp1/Program.cs
@@ -1,7 +1,10 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
 
-class Program {
-    static void Main() {
+class Program
+{
+    static void Main()
+    {
         Console.OutputEncoding = System.Text.Encoding.Unicode;
 
         Console.WriteLine("Введіть x:");
@@ -19,19 +22,32 @@ class Program {
 
     }
 
-    static int GetValidInput() {
+    static int GetValidInput()
+    {
         int coefficient;
-        while (!int.TryParse(Console.ReadLine()
-                .Replace(',', '.'), NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out coefficient)){
-            Console.WriteLine("Помилка введення. Введіть число знову:");
+        while (true)
+        {
+            string input = Console.ReadLine();
+            input = input.Replace(',', '.');
+            if (double.TryParse(input, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out double temp))
+            {
+                coefficient = (int)temp;
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Помилка введення. Введіть ціле число знову:");
+            }
         }
         return coefficient;
     }
 
-    static double Calculate(int x, int y, int z){
-        double a = 2 * Math.Cos(x*x)-0.5;
-        double b = 0.5 + Math.Sin(y^(2-z));
-        double c = z*z/(7-z/3);
-        return a/b+c;
+
+    static double Calculate(int x, int y, int z)
+    {
+        double a = 2 * Math.Cos(x * x) - 0.5;
+        double b = 0.5 + Math.Sin(Math.Pow(y, 2 - z));
+        double c = z * z / (7 - z / 3);
+        return a / b + c;
     }
 }
--- OOP/lab_01/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ OOP/lab_01/ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21b12727bcafa3143a9fa3f568173c0360dc6a5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
