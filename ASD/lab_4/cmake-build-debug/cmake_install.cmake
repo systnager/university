8ddbce2b9f55a0601d0b459ff3e3883cf48dca98
systnager <systnager@gmail.com>
2024-04-27 22:54:17 +0300

    complete lab_04 from ASD

--- ASD/lab_4/cmake-build-debug/cmake_install.cmake
+++ ASD/lab_4/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/university/ASD/lab_4
+# Install script for directory: C:/Users/systnager/Documents/university/ASD/lab_4
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/university/ASD/lab_4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/systnager/Documents/university/ASD/lab_4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- ASD/lab_4/main.cpp
+++ ASD/lab_4/main.cpp
@@ -2,74 +2,8 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <cmath>
 
-
-#define MAX_SIZE 100
-
-struct Stack {
-    double items[MAX_SIZE];
-    int top;
-};
-
-void initialize(struct Stack* stack) {
-    stack->top = -1;
-}
-
-int isEmpty(struct Stack* stack) {
-    return stack->top == -1;
-}
-
-int isFull(struct Stack* stack) {
-    return stack->top == MAX_SIZE - 1;
-}
-
-void push(struct Stack* stack, double value) {
-    if (isFull(stack)) {
-        printf("stack overflow\n");
-        return;
-    }
-    stack->items[++stack->top] = value;
-}
-
-double pop(struct Stack* stack) {
-    if (isEmpty(stack)) {
-        printf("stack empty\n");
-        return -1;
-    }
-    return stack->items[stack->top--];
-}
-
-double evaluateRPN(char expression[]) {
-    struct Stack stack;
-    initialize(&stack);
-
-    char* token = strtok(expression, " ");
-    while (token != NULL) {
-        if (isdigit(token[0])) {
-            push(&stack, atof(token)); // Перетворення рядка в число
-        } else {
-            double operand2 = pop(&stack);
-            double operand1 = pop(&stack);
-            if (strcmp(token, "+") == 0) {
-                push(&stack, operand1 + operand2);
-            } else if (strcmp(token, "-") == 0) {
-                push(&stack, operand1 - operand2);
-            } else if (strcmp(token, "*") == 0) {
-                push(&stack, operand1 * operand2);
-            } else if (strcmp(token, "/") == 0) {
-                push(&stack, operand1 / operand2);
-            } else if (strcmp(token, "^") == 0) {
-                push(&stack, pow(operand1, operand2));
-            } else if (strcmp(token, "sqrt") == 0) {
-                push(&stack, sqrt(operand2));
-            }
-        }
-        token = strtok(NULL, " ");
-    }
-
-    return pop(&stack);
-}
+using namespace std;
 
 struct Node {
     int data;
@@ -79,6 +13,30 @@ struct Node {
 
 struct Node* createEmptyList() {
     return NULL;
+}
+
+void push(struct Node** head_ref, int new_data) {
+    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
+    new_node->data = new_data;
+    new_node->prev = NULL;
+    new_node->next = *head_ref;
+    if (*head_ref != NULL)
+        (*head_ref)->prev = new_node;
+    *head_ref = new_node;
+}
+
+int pop(struct Node** head_ref) {
+    if (*head_ref == NULL) {
+        printf("Error: stack underflow\n");
+        return -1;
+    }
+    int popped_data = (*head_ref)->data;
+    struct Node* temp = *head_ref;
+    *head_ref = (*head_ref)->next;
+    if (*head_ref != NULL)
+        (*head_ref)->prev = NULL;
+    free(temp);
+    return popped_data;
 }
 
 void addToFront(struct Node** head_ref, int new_data) {
@@ -107,16 +65,6 @@ void addToEnd(struct Node** head_ref, int new_data) {
     new_node->prev = last;
 }
 
-void removeFromFront(struct Node** head_ref) {
-    if (*head_ref == NULL)
-        return;
-    struct Node* temp = *head_ref;
-    *head_ref = (*head_ref)->next;
-    if (*head_ref != NULL)
-        (*head_ref)->prev = NULL;
-    free(temp);
-}
-
 void removeFromEnd(struct Node** head_ref) {
     if (*head_ref == NULL)
         return;
@@ -128,28 +76,6 @@ void removeFromEnd(struct Node** head_ref) {
     else
         *head_ref = NULL;
     free(last);
-}
-
-void removeNode(struct Node** head_ref, struct Node* del) {
-    if (*head_ref == NULL || del == NULL)
-        return;
-    if (*head_ref == del)
-        *head_ref = del->next;
-    if (del->next != NULL)
-        del->next->prev = del->prev;
-    if (del->prev != NULL)
-        del->prev->next = del->next;
-    free(del);
-}
-
-struct Node* search(struct Node* head, int key) {
-    struct Node* current = head;
-    while (current != NULL) {
-        if (current->data == key)
-            return current;
-        current = current->next;
-    }
-    return NULL;
 }
 
 void printList(struct Node* head) {
@@ -210,11 +136,31 @@ void task_1() {
 }
 
 void task_2() {
-    char expression[100];
-    printf("enter your expression: ");
-    fgets(expression, 100, stdin);
-    double result = evaluateRPN(expression);
-    printf("result: %.2f\n", result);
+    char expression[] = "7 5 2 - 4 * +";
+    struct Node* stack = createEmptyList();
+
+    char* token = strtok(expression, " ");
+    while (token != NULL) {
+        if (isdigit(token[0]) || (token[0] == '-' && isdigit(token[1]))) {
+            push(&stack, atoi(token));
+        } else {
+            int operand2 = pop(&stack);
+            int operand1 = pop(&stack);
+            if (strcmp(token, "+") == 0) {
+                push(&stack, operand1 + operand2);
+            } else if (strcmp(token, "-") == 0) {
+                push(&stack, operand1 - operand2);
+            } else if (strcmp(token, "*") == 0) {
+                push(&stack, operand1 * operand2);
+            } else if (strcmp(token, "/") == 0) {
+                push(&stack, operand1 / operand2);
+            }
+        }
+        token = strtok(NULL, " ");
+    }
+
+    int result = pop(&stack);
+    printf("Result: %d\n", result);
 }
 
 int main() {
