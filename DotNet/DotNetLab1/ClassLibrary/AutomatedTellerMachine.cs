052411782746af3097cd6bbd35f9af7d3fe95f93
Taras Nahornyi <systnager@gmail.com>
2024-10-18 15:28:41 +0300

    complete console part of lab_01 from DotNet

---
+++ DotNet/DotNetLab1/ClassLibrary/AutomatedTellerMachine.cs
@@ -0,0 +1,34 @@
+﻿namespace ClassLibrary;
+
+public class AutomatedTellerMachine
+{
+    public string ATMId { get; set; }
+    public string Location { get; set; }
+    public decimal CashAvailable { get; private set; }
+
+    public AutomatedTellerMachine(string atmId, string location, decimal initialCash)
+    {
+        ATMId = atmId;
+        Location = location;
+        CashAvailable = initialCash;
+    }
+
+    public void ProcessWithdrawal(Account account, decimal amount)
+    {
+        if (CashAvailable >= amount)
+        {
+            account.Withdraw(amount);
+            CashAvailable -= amount;
+        }
+        else
+        {
+            throw new InvalidOperationException("Недостатньо готівки у банкоматі.");
+        }
+    }
+
+    public void ProcessDeposit(Account account, decimal amount)
+    {
+        account.Deposit(amount);
+        CashAvailable += amount;
+    }
+}
